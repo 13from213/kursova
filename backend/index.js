dc416f03e803a0f0b22e66c56682cb766b770ffe
13from213 <bogdansumy344@gmail.com>
2024-12-06 11:22:18 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import cors from 'cors'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const DB_URL = 'mongodb://localhost:27017/React';
+const DB_URL = 'mongodb+srv://bogdansumy344:Mzn08UpiTUIbv3iL@cluster0.yvi0d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const PORT = 5000;
 
 const app = express();
